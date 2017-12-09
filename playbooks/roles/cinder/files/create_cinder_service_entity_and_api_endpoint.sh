fe6967c2f0ecac529932d084c13125e2791bfe6c
hidekazuna <hidekazuna@gmail.com>
2017-12-09 11:19:41 +0900

    Update cinder and block role

--- playbooks/roles/cinder/files/create_cinder_service_entity_and_api_endpoint.sh
+++ playbooks/roles/cinder/files/create_cinder_service_entity_and_api_endpoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 openstack user create --domain default --password $1 cinder
 openstack role add --project service --user cinder admin
-openstack service create --name cinder --description "OpenStack Block Storage" volume
 openstack service create --name cinderv2 --description "OpenStack Block Storage" volumev2
-openstack endpoint create --region RegionOne volume public http://$2:8776/v1/%\(tenant_id\)s
-openstack endpoint create --region RegionOne volume internal http://$2:8776/v1/%\(tenant_id\)s
-openstack endpoint create --region RegionOne volume admin http://$2:8776/v1/%\(tenant_id\)s
-openstack endpoint create --region RegionOne volumev2 public http://$2:8776/v2/%\(tenant_id\)s
-openstack endpoint create --region RegionOne volumev2 internal http://$2:8776/v2/%\(tenant_id\)s
-openstack endpoint create --region RegionOne volumev2 admin http://$2:8776/v2/%\(tenant_id\)s
+openstack service create --name cinderv3 --description "OpenStack Block Storage" volumev3
+openstack endpoint create --region RegionOne volumev2 public http://$2:8776/v2/%\(project_id\)s
+openstack endpoint create --region RegionOne volumev2 internal http://$2:8776/v2/%\(project_id\)s
+openstack endpoint create --region RegionOne volumev2 admin http://$2:8776/v2/%\(project_id\)s
+openstack endpoint create --region RegionOne volumev3 public http://$2:8776/v3/%\(project_id\)s
+openstack endpoint create --region RegionOne volumev3 internal http://$2:8776/v3/%\(project_id\)s
+openstack endpoint create --region RegionOne volumev3 admin http://$2:8776/v3/%\(project_id\)s
