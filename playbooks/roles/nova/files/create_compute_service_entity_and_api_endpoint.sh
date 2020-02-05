3827890250bf5046d4adb694751cadaf5f88cfb9
Hidekazu Nakamura <hidekazuna@gmail.com>
2020-02-05 07:13:30 +0000

    Update to use https endpoint

--- playbooks/roles/nova/files/create_compute_service_entity_and_api_endpoint.sh
+++ playbooks/roles/nova/files/create_compute_service_entity_and_api_endpoint.sh
@@ -2,6 +2,6 @@
 openstack user create --domain default --password $1 nova
 openstack role add --project service --user nova admin
 openstack service create --name nova --description "OpenStack Compute" compute
-openstack endpoint create --region RegionOne compute public http://$2:8774/v2.1
-openstack endpoint create --region RegionOne compute internal http://$2:8774/v2.1
-openstack endpoint create --region RegionOne compute admin http://$2:8774/v2.1
+openstack endpoint create --region RegionOne compute public $2://$3:8774/v2.1
+openstack endpoint create --region RegionOne compute internal $2://$3:8774/v2.1
+openstack endpoint create --region RegionOne compute admin $2://$3:8774/v2.1
